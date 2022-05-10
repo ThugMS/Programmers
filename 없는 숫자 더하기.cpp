7a3ec69177d5947ee6c292960dde503602b2fa18
ThugMS <leems041625@gmail.com>
2022-05-11 01:29:31 +0900

    음양 더하기 solve

--- 없는 숫자 더하기.cpp
+++ 없는 숫자 더하기.cpp
@@ -1,20 +1,20 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int solution(vector<int> numbers) {
-    int answer = 0;
-    bool check[10] = {false, };
-    
-    for(int i=0;i<numbers.size();i++){
-        check[numbers[i]] = true;
-    }
-    
-    for(int i=0;i<10;i++){
-        if(check[i] == false)
-            answer += i;
-    }
-    
-    return answer;
-}
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(vector<int> numbers) {
+//    int answer = 0;
+//    bool check[10] = {false, };
+//    
+//    for(int i=0;i<numbers.size();i++){
+//        check[numbers[i]] = true;
+//    }
+//    
+//    for(int i=0;i<10;i++){
+//        if(check[i] == false)
+//            answer += i;
+//    }
+//    
+//    return answer;
+//}
