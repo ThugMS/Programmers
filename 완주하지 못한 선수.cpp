e00047c7be4cb88db273e3810ae7ceb15aaa5542
MSLEE <leems041625@gmail.com>
2022-05-27 14:14:15 +0900

    [feat] 내적, 완주하지 못한선수 solve

---
+++ 완주하지 못한 선수.cpp
@@ -0,0 +1,30 @@
+//#include <string>
+//#include <vector>
+//#include <unordered_map>
+//
+//using namespace std;
+//
+//string solution(vector<string> participant, vector<string> completion) {
+//    string answer = "";
+//    unordered_map<string, int> com;
+//
+//    for (int i = 0; i < completion.size(); i++) {
+//        if (com[completion[i]] >= 1) {
+//            com[completion[i]]++;
+//        }
+//        else
+//            com[completion[i]] = 1;
+//    }
+//
+//    for (int i = 0; i < participant.size(); i++) {
+//        if (com[participant[i]] >= 1) {
+//            com[participant[i]]--;
+//        }
+//        else if (com[participant[i]] <= 0) {
+//            answer += participant[i];
+//            break;
+//        }
+//    }
+//
+//    return answer;
+//}
