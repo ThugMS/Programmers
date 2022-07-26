d6965859f1bab99c22b7dd36f79d788262372972
MSLEE <leems041625@gmail.com>
2022-07-26 22:19:38 +0900

    1111

--- skt코테 1번.cpp
+++ skt코테 1번.cpp
@@ -1,36 +1,36 @@
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//
-//vector<int> solution(vector<int> p) {
-//    vector<int> answer;
-//    vector<int> num(1001, 0);
-//
-//    int min_;
-//    int index;
-//
-//    for (int i = 0; i < p.size() - 1; i++) {
-//        min_ = p[i];
-//        for (int j = i+1; j < p.size(); j++) {
-//            if (min_ > p[j]) {
-//                index = j;
-//                min_ = p[j];
-//            }
-//        }
-//        if (min_ != p[i]) {
-//            int sub = p[i];
-//            p[i] = min_;
-//            p[index] = sub;
-//
-//            num[i]++;
-//            num[index]++;
-//        }
-//    }
-//
-//    for (int i = 0; i < p.size(); i++) {
-//        answer.push_back(num[i]);
-//    }
-//
-//    return answer;
-//}
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> p) {
+    vector<int> answer;
+    vector<int> num(1001, 0);
+
+    int min_;
+    int index;
+
+    for (int i = 0; i < p.size() - 1; i++) {
+        min_ = p[i];
+        for (int j = i+1; j < p.size(); j++) {
+            if (min_ > p[j]) {
+                index = j;
+                min_ = p[j];
+            }
+        }
+        if (min_ != p[i]) {
+            int sub = p[i];
+            p[i] = min_;
+            p[index] = sub;
+
+            num[i]++;
+            num[index]++;
+        }
+    }
+
+    for (int i = 0; i < p.size(); i++) {
+        answer.push_back(num[i]);
+    }
+
+    return answer;
+}
