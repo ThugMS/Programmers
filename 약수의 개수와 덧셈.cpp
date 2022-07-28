646bdce6c239727d48bda807e0576f53c53f460f
MSLEE <leems041625@gmail.com>
2022-07-28 22:27:59 +0900

    dd

--- 약수의 개수와 덧셈.cpp
+++ 약수의 개수와 덧셈.cpp
@@ -1,27 +1,27 @@
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//bool checking(int n) {
-//    for (int i = 0; i <= n; i++) {
-//        if (i * i > n)
-//            return false;
-//        else if (i * i == n)
-//            return true;
-//    }
-//    return false;
-//}
-//int solution(int left, int right) {
-//    int answer = 0;
-//
-//    for (int i = left; i <= right; i++) {
-//        if (checking(i)) {
-//            answer -= i;
-//        }
-//        else {
-//            answer += i;
-//        }
-//    }
-//
-//    return answer;
-//}
+#include <string>
+#include <vector>
+
+using namespace std;
+bool checking(int n) {
+    for (int i = 0; i <= n; i++) {
+        if (i * i > n)
+            return false;
+        else if (i * i == n)
+            return true;
+    }
+    return false;
+}
+int solution(int left, int right) {
+    int answer = 0;
+
+    for (int i = left; i <= right; i++) {
+        if (checking(i)) {
+            answer -= i;
+        }
+        else {
+            answer += i;
+        }
+    }
+
+    return answer;
+}
