9bbf5404d403c4a727593f14aa60c0b6daef519c
MSLEE <leems041625@gmail.com>
2022-08-01 22:26:14 +0900

    입국심사.cpp

--- 입국심사.cpp
+++ 입국심사.cpp
@@ -1,34 +1,34 @@
-//#include <string>
-//#include <vector>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//long long solution(int n, vector<int> times) {
-//    long long answer = 0;
-//
-//    sort(times.begin(), times.end());
-//
-//    long long start = 1;
-//    long long end = (long long)times[times.size() - 1] * (long long)n;
-//
-//    while (start <= end) {
-//        long long mid = (start + end) / 2;
-//        long long cnt = 0;
-//
-//        for (int i = 0; i < times.size(); i++) {
-//            cnt += mid / times[i];
-//        }
-//
-//        if (cnt < n) {
-//            start = mid + 1;
-//        }
-//        else {
-//            answer = mid;
-//            end = mid - 1;
-//        }
-//        
-//    }
-//
-//    return answer;
-//}
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+
+    sort(times.begin(), times.end());
+
+    long long start = 1;
+    long long end = (long long)times[times.size() - 1] * (long long)n;
+
+    while (start <= end) {
+        long long mid = (start + end) / 2;
+        long long cnt = 0;
+
+        for (int i = 0; i < times.size(); i++) {
+            cnt += mid / times[i];
+        }
+
+        if (cnt < n) {
+            start = mid + 1;
+        }
+        else {
+            answer = mid;
+            end = mid - 1;
+        }
+        
+    }
+
+    return answer;
+}
