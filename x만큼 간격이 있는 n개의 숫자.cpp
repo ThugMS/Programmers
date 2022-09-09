897c546414fc7b6b4b515266a92c04ed35d41255
ThugMS <leems041625@gmail.com>
2022-09-09 16:49:59 +0900

    [Feat] : x만큼 간격이 있는 n개의 숫자

---
+++ x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,15 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<long long> solution(int x, int n) {
+//    vector<long long> answer;
+//    
+//    for(int i=1;i<=n;i++){
+//        long long value = x * i;
+//        answer.push_back(value);
+//    }
+//    
+//    return answer;
+//}
