9cc20e140fb0576dda7c27cb9be49371b9e4d34f
ThugMS <leems041625@gmail.com>
2022-09-09 16:58:17 +0900

    [Feat] : 콜라츠 추측

---
+++ 콜라츠 추측.cpp
@@ -0,0 +1,21 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int num) {
+//    int cnt = 0;
+//    
+//    while(num != 1){
+//        if(cnt >= 450)
+//            return -1;
+//        
+//        if(num % 2 == 0)
+//            num /= 2;
+//        else
+//            num = (num * 3) + 1;
+//        
+//        cnt++;
+//    }
+//    return cnt;
+//}
