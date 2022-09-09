cdfd930445b6dd22d9e74efa7327ef8b47c9b61d
ThugMS <leems041625@gmail.com>
2022-09-09 16:15:27 +0900

    [Feat] : 정수 제곱근 판별

---
+++ 정수 제곱근 판별.cpp
@@ -0,0 +1,18 @@
+//#include <string>
+//#include <vector>
+//#include <cmath>
+//
+//using namespace std;
+//
+//long long solution(long long n) {
+//    long long answer = 0;
+//    
+//    double sub = sqrt(n);
+//    
+//    if(sub == (long long)sub)
+//        answer = pow(sub + 1, 2);
+//    else
+//        answer = -1;
+//    
+//    return answer;
+//}
