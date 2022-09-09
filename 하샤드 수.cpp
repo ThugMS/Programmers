033fd534220898f0f757bdee36528c75df7a21a3
ThugMS <leems041625@gmail.com>
2022-09-09 16:39:50 +0900

    [Feat] : 하샤드 수

---
+++ 하샤드 수.cpp
@@ -0,0 +1,20 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//bool solution(int x) {
+//    bool answer = true;
+//    
+//    int sub = x;
+//    int sum = 0;
+//    
+//    while(sub != 0){
+//        sum += sub % 10;
+//        sub /= 10;
+//    }
+//    
+//    answer = x % sum == 0;
+//
+//    return answer;
+//}
