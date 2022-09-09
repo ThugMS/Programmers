28a1c100b58c6cd0e773e59ab0c55980438677e8
ThugMS <leems041625@gmail.com>
2022-09-09 16:25:04 +0900

    [Feat] : 문자열 내 p와 y의 개수

---
+++ 문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,24 @@
+//#include <string>
+//#include <iostream>
+//using namespace std;
+//
+//bool solution(string s)
+//{
+//    bool answer = true;
+//    
+//    int cntP = 0;
+//    int cntY = 0;
+//    
+//    for(int i = 0; i < s.length(); i++){
+//        if(s[i] == 'P' || s[i] == 'p')
+//            cntP++;
+//        
+//        if(s[i] == 'Y' || s[i] == 'y')
+//            cntY++;
+//        
+//    }
+//    
+//    answer = cntP == cntY;
+//    
+//    return answer;
+//}
