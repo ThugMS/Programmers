401b9c51ca146572f5d15a5b68855adf08257d4e
ThugMS <leems041625@gmail.com>
2022-09-12 13:45:46 +0900

    [Feat] : 서울에서 김서방 찾기

---
+++ 서울에서 김서방 찾기.cpp
@@ -0,0 +1,23 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(vector<string> seoul) {
+//    string answer = "";
+//    int answerNum = 0;
+//    
+//    for(int i = 0; i < seoul.size(); i++){
+//        if(seoul[i] == "Kim"){
+//            answerNum = i;
+//            break;
+//        }
+//    }
+//    
+//    answer += "김서방은 ";
+//    answer += to_string(answerNum);
+//    answer += "에 있다";
+//    
+//    return answer;
+//}
+//
