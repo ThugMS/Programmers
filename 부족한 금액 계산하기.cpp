dd9c76042a0a1baf272824093ac6c77e50a1c82e
MSLEE <leems041625@gmail.com>
2022-09-14 00:14:55 +0900

    [Feat] : 부족한 금액 계산하기

---
+++ 부족한 금액 계산하기.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//long long solution(int price, int money, int count)
+//{
+//    long long sum = 0;
+//    long long cnt = 1;
+//    
+//    for (int i = 1; i <= count; i++) {
+//        sum += price * i;
+//    }
+//
+//    if (sum > money)
+//        return sum - money;
+//    else
+//        return 0;
+//}
