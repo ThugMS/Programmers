280f49a73c2fcf87b07efdecb76c3ae5b58ca20f
ThugMS <leems041625@gmail.com>
2022-09-14 12:02:09 +0900

    [Feat] : 피보나치수

---
+++ 피보나치 수.cpp
@@ -0,0 +1,45 @@
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//
+//unsigned long long Fibo(int n){
+//    unsigned long long a,b;
+//    unsigned long long value;
+//    int i;
+//    
+//    a = 1;
+//    b = 0;
+//    i = 0;
+//    value = 0;
+//    while (i < n - 1)
+//    {
+//        value = (a + b)% 1234567;;
+//        b = a;
+//        a = value;
+//        i++;
+//    }
+//    cout << i << endl;
+//    return value;
+//}
+//
+//int solution(int n) {
+//    int answer = 0;
+//    
+//    unsigned long long sub;
+//    
+//    sub = Fibo(n);
+//    
+//    answer = sub % 1234567;
+//    
+//    return answer;
+//}
+//
+//int main(){
+//    
+//    
+//    cout << solution(100) << endl;
+//}
+//
