77819c777e6a993a6c39efa29dbc0ca7ab7b5924
ThugMS <leems041625@gmail.com>
2022-09-14 09:49:46 +0900

    [Feat] : 같은 숫자는 싫어

---
+++ 같은 숫자는 싫어.cpp
@@ -0,0 +1,24 @@
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> arr)
+//{
+//    vector<int> answer;
+//    
+//    int prev = -1;
+//    
+//    for(int i=0;i<arr.size();i++){
+//        if(arr[i] == prev)
+//            continue;
+//        else{
+//            prev = arr[i];
+//            answer.push_back(arr[i]);
+//            continue;
+//        }
+//            
+//    }
+//
+//    return answer;
+//}
