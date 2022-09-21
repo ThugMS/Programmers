428289d18763b90b742dd1c4c24d4701129ffc49
ThugMS <leems041625@gmail.com>
2022-09-21 14:39:07 +0900

    [Feat] : 문자열 내 마음대로 정렬하기

---
+++ 문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,23 @@
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int indexValue = 0;
+//
+//bool SortIndex(string a, string b){
+//    if(a[indexValue] == b[indexValue])
+//        return a<b;
+//    return a[indexValue] < b[indexValue];
+//}
+//
+//vector<string> solution(vector<string> strings, int n) {
+//    vector<string> answer;
+//    
+//    indexValue = n;
+//    
+//    sort(strings.begin(), strings.end(), SortIndex);
+//    
+//    return strings;
+//}
