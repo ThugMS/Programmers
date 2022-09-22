9560d81f3cccd3a0acb537949f922575faed10fc
ThugMS <leems041625@gmail.com>
2022-09-22 14:11:08 +0900

    [Feat] : 점프와 순간 이동

---
+++ 점프와 순간 이동.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+//int solution(int n)
+//{
+//    int ans = 1;
+//    
+//    while(n != 1){
+//        if(n % 2 == 0){
+//            n /= 2;
+//        }
+//        else{
+//            n -= 1;
+//            n /= 2;
+//            ans++;
+//        }
+//    }
+//
+//    return ans;
+//}
+//
+//
