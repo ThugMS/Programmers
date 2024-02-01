1f65eac19c11d9fcf8a3ef08edc770a755d6e8c6
MSLEE <39873890+ThugMS@users.noreply.github.com>
2024-02-02 01:25:00 +0900

    [Silver III] Title: 타노스, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.cc
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -2,6 +2,41 @@
 
 using namespace std;
 
+// 25 점 서브태스크1
+//int main() {
+//
+//	string str;
+//
+//	cin >> str;
+//
+//	int len = str.length();
+//
+//	int odd = len / 2;
+//	int even = len / 2;
+//
+//	odd /= 2;
+//
+//	even /= 2;
+//
+//	string answer;
+//
+//	int cnt = 0;
+//
+//	//cout << len << endl;
+//
+//	for (int i = 1; i <= len / 2; i++) {
+//		if (cnt < even)
+//			answer += "0";
+//		else
+//			answer += "1";
+//		
+//		cnt++;
+//	}
+//
+//	cout << answer << endl;
+//
+//	return 0;
+//}
 
 int main() {
 
@@ -9,31 +44,48 @@ int main() {
 
 	cin >> str;
 
-	int len = str.length();
+	long long len = str.length();
+	long long odd = 0;
+	long long even = 0;
 
-	int odd = len / 2;
-	int even = len / 2;
-
+	for (long long  i = 0; i < str.length(); i++) {
+		if (str[i] == '1') {
+			odd++;
+		}
+		else {
+			even++;
+		}
+	}
+	
 	odd /= 2;
-
 	even /= 2;
 
-	string answer;
+	long long cntOdd = 0;
+	long long cntEven = 0;
 
-	int cnt = 0;
+	for (long long i = 0; i < str.length(); i++) {
+		if (cntOdd == odd)
+			break;
 
-	//cout << len << endl;
-
-	for (int i = 1; i <= len / 2; i++) {
-		if (cnt < even)
-			answer += "0";
-		else
-			answer += "1";
-		
-		cnt++;
+		if (str[i] == '1') {
+			str.erase(str.begin() + i);
+			cntOdd++;
+			i--;
+		}
 	}
 
-	cout << answer << endl;
+	for (long long i = str.length() - 1; i >= 0; i--) {
+		if (cntEven == even)
+			break;
+
+		if (str[i] == '0') {
+			str.erase(str.begin() + i);
+			cntEven++;
+			i++;
+		}
+	}
+
+	cout << str << endl;
 
 	return 0;
 }
